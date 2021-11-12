6fe67699cec52ce55c56be7be2bca98000de07c3
Bilel <bilel@powder.gg>
2021-11-12 15:06:31 +0100

    Initial Commit

---
+++ Shared/FitnessAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitnessAppApp.swift
+//  Shared
+//
+//  Created by Bilel Bouricha on 12/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct FitnessAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
